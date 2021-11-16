8cbfa3f9a04f39f9e3d75a4df55ed2908fb229b1
AntonBogun <bogunanton@gmail.com>
2021-11-16 23:51:30 +0200

    MMm icon

--- app/src/main/java/com/example/productdatabase/Main.java
+++ app/src/main/java/com/example/productdatabase/Main.java
@@ -9,4 +9,5 @@ public class Main extends android.app.Application{
     public int global_i=0;
     public ArrayList<Integer> global_l=new ArrayList<Integer>();
     //public String name=getApplicationName(this);
+    //http://www.developerphil.com/dont-store-data-in-the-application-object/
 }
